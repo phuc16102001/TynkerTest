4a32d31e95c5d7d9c0591c72b57af5a333fa3765
phuc16102001 <phuc16102001@gmail.com>
2021-05-23 13:52:32 +0700

    New file

---
+++ docs/data/slide7.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{101E560C-6999-4682-AA11-7807D63AE553}'];loadHandler&&loadHandler(6, '<div id="spr0_70b99a"><div id="spr1_70b99a" class="kern slide"><img id="img0_70b99a" src="data/img3.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_70b99a" class="kern slide"><div id="wo_6_0"></div></div></div>', '{"s":[]}');})();
